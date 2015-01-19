1c5fc3b6195c72d67371f6aa6fa7713e76f175ce
Pino <pino.jose@gmail.com>
2015-01-19 08:43:23 -0430

    Archivos básicos para migración

---
+++ header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>CyA, Controles y Automatización</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="styles.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<style>
+      #map-container { height: 300px }
+    </style>
+  </head>
+
+  <body>
+
+    <div class="container">
+      <!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">            
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="./">Inicio <span class="sr-only">(current)</span></a></li>
+              <li><a href="../navbar-static-top/">Nosotros</a></li>
+              <li><a href="../navbar-fixed-top/">Aliados</a></li>
+      			  <li><a href="../navbar-fixed-top/">Proyectos</a></li>
+      			  <li><a href="../navbar-fixed-top/">Portafolio</a></li>
+      			  <li><a href="../navbar-fixed-top/">Clientes</a></li>
+      			  <li><a href="../navbar-fixed-top/">Blog</a></li>
+      			  <li><a href="../navbar-fixed-top/">Contactos</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
---
+++ index.php
@@ -0,0 +1,126 @@
+    <?php get_header(); ?>
+	  <div class="backgroundTop">
+	  
+	  </div>
+      <!-- Main component for a primary marketing message or call to action -->
+	  <section class="contentPage">
+        <div class="row">
+            <div class="col-xs-12 col-md-6">
+        			<h1>CONTROLES Y AUTOMATIZACIÓN</h1>
+        			<p>Es una empresa presente en el mercado de la integración de proyectos y servicios para la industria colombiana desde el año 2002. Nuestro selecto grupo de profesionales especializados en diferentes áreas de la ingeniería cuenta con un amplio conocimiento y una excelente experiencia en cada uno de los campos de acción que desarrollamos.</p>
+            </div>
+            <div class="col-xs-12 col-md-6">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                    </ol>
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                      <div class="item active">
+                        <img src="img/automatiza.jpg" alt="...">
+                        <div class="carousel-caption">
+                          ...
+                        </div>
+                      </div>
+                      <div class="item">
+                        <img src="img/energia.jpg" alt="...">
+                        <div class="carousel-caption">
+                          ...
+                        </div>
+                      </div>
+                      <div class="item">
+                        <img src="img/telecomunicaciones.jpg" alt="...">
+                        <div class="carousel-caption">
+                          ...
+                        </div>
+                      </div>
+					            <div class="item">
+                        <img src="img/syc.jpg" alt="...">
+                        <div class="carousel-caption">
+                          ...
+                        </div>
+                      </div>
+                    </div>
+
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+            </div>
+        </div>
+        <div class="visible-lg banner">
+          <img src="img/banner.png">
+        </div>
+	   </section>
+     <section class="lineas">
+        <div class="row">
+          <div class="col-xs-12 col-md-4">
+            <div class="cuadrosBlancos">
+              <span class="fa fa fa-suitcase fa-5x"></span>
+              <h4>LÍNEAS DE TRABAJO</h3>
+              <p>
+                Nuestro oferta de valor abarca distintas áreas de la ingeniería eléctrica y electrónica, donde aplicamos tecnología de punta para solucionar las necesidades de nuestros clientes.
+              </p>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <div class="cuadrosBlancos">
+              <span class="fa fa fa-cubes fa-5x"></span>
+              <h4>Proyectos:</h3>
+              <p>
+                Desarrollamos proyectos con filosofía PMI, asegurando una correcta planeación y ejecución en cualquier etapa de los mismos.
+              </p>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <div class="cuadrosBlancos">
+              <span class="fa fa fa-cogs fa-5x"></span>
+              <h4>SERVICIOS</h3>
+              <p>
+                Garantizamos la continuidad de sus procesos, minimizando errores, pérdidas, paradas, accidentes y en general cualquier evento de falla que vaya en contra del correcto funcionamiento de los sistemas que apuntalan su core de negocios.
+              </p>
+            </div>
+          </div>
+        </div>
+        <hr>
+     </section>
+	 <section class="ultimosPost">
+		<h4>Proyectos ejecutados:</h4>
+		<div class="row">
+			<div class="col-xs-12 col-md-3">
+				<div class="post">
+					<img src="http://placehold.it/550x350" class="img-thumbnail img-responsive">
+					<p><strong>Coca Cola Latinoamérica</strong></p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-3">
+				<div class="post">
+					<img src="http://placehold.it/550x350" class="img-thumbnail img-responsive">
+					<p><strong>Colegio Rochester</strong></p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-3">
+				<div class="post">
+					<img src="http://placehold.it/550x350" class="img-thumbnail img-responsive">
+					<p><strong>Sala de audiencia CSJ</strong></p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-3">
+				<div class="post">
+					<img src="http://placehold.it/550x350" class="img-thumbnail img-responsive">
+					<p><strong>Superintendencia de Sociedades</strong></p>
+				</div>
+			</div>	
+		</div>
+	 </section>
+	 <?php get_footer(); ?>
