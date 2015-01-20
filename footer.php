d20d29eb8d84159eeb12ee0d0166cb946ecb249a
Jose Pino <pino.jose@gmail.com>
2015-01-20 08:32:05 -0430

    agregados js de bootstrap y jQuery

--- footer.php
+++ footer.php
@@ -11,45 +11,6 @@
 	  </section>
     </div> <!-- /container -->
 	
-	
-	<!--	
-	<div class="footer">
-		<div class="container">
-          <div class="col-xs-12 col-md-6">
-		  </div>
-		  <div class="col-xs-12 col-md-6">
-			<div id="map-container"></div>
-		  </div>
-		</div>
-	</div>-->
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-	<!--
-	<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
-    <script>	
- 
-      function initialize() {
-			  var myLatlng = new google.maps.LatLng(4.688943, -74.066886);
-			  var mapOptions = {
-				zoom: 17,
-				center: myLatlng
-			  }
-			  var map = new google.maps.Map(document.getElementById('map-container'), mapOptions);
-
-			  var marker = new google.maps.Marker({
-				  position: myLatlng,
-				  map: map,
-				  title: 'info@cya-sa.com / PBX: +57 (1) 643 8360 / Carrera 61 Bis No. 99 – 88 Bogotá D.C.'
-			  });
-			}
-
-			google.maps.event.addDomListener(window, 'load', initialize);
- 
-    </script>-->
+    <?php wp_footer(); ?>
   </body>
 </html>
